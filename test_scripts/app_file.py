6b19ccd794476bd8bd01b8015ea002b15462a83d
MrPain-hub <lix961@outlook.com>
2024-10-10 17:48:13 +0300

    work241010

--- test_scripts/app_file.py
+++ test_scripts/app_file.py
@@ -1,13 +1,186 @@
-import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 import tkinter as tk
-from tkinter import filedialog, ttk, messagebox
+from tkinter import ttk
 
 from datetime import date, datetime, timedelta
 from os import startfile, path, makedirs
 import sys
+import pickle
+
+from geotechnical_solutions.GeoSimModel.create_models.geology_models import *
+from geotechnical_solutions.GeoSimModel.create_models.foundation_models import *
+from geotechnical_solutions.GeoSimModel.solve.building_analysis import *
+from geotechnical_solutions.GeoSimModel import path_data
+
+class MainWindow(ttk.Frame):
+    """
+    Основное окно
+    """
+    def __init__(self, root=None):
+        super().__init__(root)
+        self.root = root
+        self.root.title("Geotechnical Solutions")
+        self.base_path = self.resource_path("picture")
+        self.root.iconbitmap(path.join(self.base_path, "my_icon.ico"))
+        self.__add_menu(self.root)
+
+        # Создание фреймов
+        self.frame_win1 = tk.LabelFrame(self.root, text="Исходные данные", relief="raised", fg="white", bg="green")
+        self.frame_win2 = tk.LabelFrame(self.root, text="Расчеты", relief="raised", fg="white", bg="black")
+
+        self.frame_win1.pack(side="left", fill='both', padx=10, pady=10)
+        self.frame_win2.pack(side="right", fill='both', padx=10, pady=10)
+
+        ttk.Button(self.frame_win1, text="Создание скважины", command=self.create_borehole).pack(fill='both')
+        ttk.Button(self.frame_win1, text="Создание ИГЭ", command=self.create_material).pack(fill='both')
+
+        ttk.Button(self.frame_win2, text="Метод Послойного Суммирования", command=self.open_LSM).pack(fill='both')
+        ttk.Button(self.frame_win2, text="Расчет несущей способности висячих свай", command=self.open_piles).pack(fill='both')
+
+    def resource_path(self, relative_path):
+        try:
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = path.abspath(".")
+        return path.join(base_path, relative_path)
+
+    def __add_menu(self, window):
+        def open_file():
+            filepath = tk.filedialog.askopenfilename()
+
+            try:
+                with open(filepath, 'rb') as f:
+                    self.loaded_data = pickle.load(f)
+            except FileNotFoundError as ex:
+                print(ex)
+
+            print("Open file")
+
+        def save_file():
+            filepath = tk.filedialog.askopenfilename()
+
+            try:
+                with open(filepath, 'wb') as f:
+                    pickle.dump(self.loaded_data, f)
+            except Exception as ex:
+                print(ex)
+
+        def cut():
+            print("Cut")
+
+        def copy():
+            print("Copy")
+
+        def paste():
+            print("Paste")
+
+        def about():
+            print("About")
+
+        self.menu_bar = tk.Menu(window)
+
+        # Create a "File" menu
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.file_menu.add_command(label="Open", command=open_file)
+        self.file_menu.add_command(label="Save", command=save_file)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", command=window.quit)
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+
+        # Create an "Edit" menu
+        self.edit_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.edit_menu.add_command(label="Cut", command=cut)
+        self.edit_menu.add_command(label="Copy", command=copy)
+        self.edit_menu.add_command(label="Paste", command=paste)
+        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
+
+        # Create a "Help" menu
+        self.help_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.help_menu.add_command(label="About", command=about)
+        self.menu_bar.add_cascade(label="Help", menu=self.help_menu)
+
+        # Add the menu bar to the application
+        window.config(menu=self.menu_bar)
+
+    def create_borehole(self):
+        WindowCreateBorehole(tk.Toplevel(self.root))
+
+    def create_material(self):
+        WindowCreateMaterial(tk.Toplevel(self.root))
+
+
+    def open_piles(self):
+        WindowPiles(tk.Toplevel(self.root))
+
+    def open_LSM(self):
+        WindowLSM(tk.Toplevel(self.root))
+
+
+class WindowCreateBorehole(ttk.Frame):
+    """
+    Окно создания скважины
+    """
+    def __init__(self, root=None):
+        super().__init__(root)
+        self.root = root
+        self.root.title("Создание сваи")
+
+        soils_num = tk.simpledialog.askstring("Input", "Сколько слоев в скважине?")
+
+        self.frame_win1 = tk.LabelFrame(self.root, text="Параметры скважины", relief="raised", fg="white", bg="black")
+        self.frame_win1.pack(side="left", fill='both', padx=10, pady=10)
+        self.__create_borehole(self.frame_win1, int(soils_num))
+
+
+    def __create_borehole(self, frame, n=5):
+        ttk.Label(frame, text="E [Па]").grid(row=0, column=0)
+        ttk.Label(frame, text="gamma").grid(row=0, column=1)
+        ttk.Label(frame, text="Z [м]").grid(row=0, column=2)
+        ttk.Label(frame, text="water").grid(row=0, column=3)
+        for i in range(n):
+            for j in range(4):
+                entry = ttk.Entry(frame, width=10)
+                entry.insert(0, f"{i}_{j}")
+                entry.grid(row=i+1, column=j)
+
+
+class WindowCreateMaterial(ttk.Frame):
+    """
+    Окно создания материала
+    """
+    def __init__(self, root=None):
+        super().__init__(root)
+        self.root = root
+        self.root.title("Добавление ИГЭ")
+
+        frame1 = ttk.Frame(self.root)
+        frame2 = tk.LabelFrame(self.root, text="Уровень воды")
+
+        frame1.pack()
+        frame2.pack()
+
+        ttk.Label(frame1, text="E [Па]").grid(row=0, column=0)
+        ttk.Label(frame1, text="gamma").grid(row=1, column=0)
+        ttk.Label(frame1, text="Z [м]").grid(row=2, column=0)
+
+        material_list = []
+
+        for i in range(3):
+            entry = ttk.Entry(frame1, width=10)
+            entry.insert(0, f"{i}")
+            entry.grid(row=i, column=1)
+            material_list.append(entry.get())
+
+        ttk.Label(frame2, text="water").grid(row=0, column=0)
+        for i in range(2):
+            entry = ttk.Entry(frame2, width=5)
+            entry.insert(0, f"{i}")
+            entry.grid(row=0, column=i+1)
+            material_list.append(entry.get())
+
+        print(material_list)
 
 
 class WindowPiles(ttk.Frame):
@@ -100,46 +273,6 @@ class WindowLSM(ttk.Frame):
     def save_plot(self):
         fig = self.figure
         fig.savefig("last_plot.png")
-
-
-class MainWindow(ttk.Frame):
-    """
-    Основное окно
-    """
-    def __init__(self, root=None):
-        super().__init__(root)
-        self.root = root
-        self.root.title("Geotechnical Solutions")
-        self.base_path = self.resource_path("picture")
-        self.root.iconbitmap(path.join(self.base_path, "my_icon.ico"))
-
-        # Создание фреймов
-        self.frame_win1 = tk.LabelFrame(self.root, text="Исходные данные", relief="raised", fg="white", bg="green")
-        self.frame_win2 = tk.LabelFrame(self.root, text="Расчеты", relief="raised", fg="white", bg="black")
-
-        self.frame_win1.pack(side="left", fill='both', padx=10, pady=10)
-        self.frame_win2.pack(side="right", fill='both', padx=10, pady=10)
-
-        ttk.Button(self.frame_win1, text="Создание скважины", command=self.open_LSM).pack(fill='both')
-        ttk.Button(self.frame_win1, text="Создание материала", command=self.open_LSM).pack(fill='both')
-
-        ttk.Button(self.frame_win2, text="Метод Послойного Суммирования", command=self.open_LSM).pack(fill='both')
-        ttk.Button(self.frame_win2, text="Расчет несущей способности висячих свай", command=self.open_Piles).pack(fill='both')
-
-    def resource_path(self, relative_path):
-        try:
-            base_path = sys._MEIPASS
-        except Exception:
-            base_path = path.abspath(".")
-        return path.join(base_path, relative_path)
-
-
-    def open_Piles(self):
-        WindowPiles(tk.Toplevel(self.root))
-
-
-    def open_LSM(self):
-        WindowLSM(tk.Toplevel(self.root))
 
 
 if __name__ == "__main__":
